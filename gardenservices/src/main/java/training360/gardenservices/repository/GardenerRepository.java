20d039b19661b9d36f417118b9698020b8dcb337
csi9aeme <csi9aeme@gmail.com>
2023-05-19 10:45:54 +0200

    repo

--- gardenservices/src/main/java/training360/gardenservices/repository/GardenerRepository.java
+++ gardenservices/src/main/java/training360/gardenservices/repository/GardenerRepository.java
@@ -9,7 +9,10 @@ import java.util.List;
 
 public interface GardenerRepository extends JpaRepository<Gardener, Long> {
 
-    @Query("select  new training360.gardenservices.dto.NameDescriptionPairDto(gardener.name, gardenwork.description) " +
-            "from Gardener gardener inner join gardener.gardenWorks  gardenwork where gardenwork.done  = false order by gardener.name, gardenwork.description")
+//    @Query("select  new training360.gardenservices.dto.NameDescriptionPairDto(gardener.name, gardenwork.description) " +
+//            "from Gardener gardener inner join gardener.gardenWorks  gardenwork where gardenwork.done  = false order by gardener.name, gardenwork.description")
+//    List<NameDescriptionPairDto> findWorkPairs();
+
+    @Query("select  new training360.gardenservices.dto.NameDescriptionPairDto(gardenwork.gardener.name, gardenwork.description) from GardenWork gardenwork where gardenwork.done  = false order by gardenwork.gardener.name, gardenwork.description")
     List<NameDescriptionPairDto> findWorkPairs();
 }
